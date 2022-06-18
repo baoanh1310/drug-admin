270fd134a6d1652d570dd60372e5cf1fba4a9a02
baoanh1310 <dataholicguy@gmail.com>
2022-06-19 02:38:02 +0700

    add pseudo auth provider and Vaipe assets

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import { Admin, Resource, ListGuesser } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
 
 import Dashboard from './components/Dashboard';
-
+import authProvider from "./components/authProvider";
 
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 
 const App = () => (
-  <Admin dataProvider={dataProvider} dashboard={Dashboard}>
+  <Admin dataProvider={dataProvider} dashboard={Dashboard} authProvider={authProvider}>
     <Resource name="users" list={ListGuesser} />
   </Admin>
 );
