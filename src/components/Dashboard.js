9843f00e5f59f100811a5177ffb4a9630d06845c
baoanh1310 <dataholicguy@gmail.com>
2022-06-19 02:31:38 +0700

    add simple homepage and pseudo user tab

---
+++ src/components/Dashboard.js
@@ -0,0 +1,11 @@
+import * as React from "react";
+import { Card, CardContent, CardHeader } from '@mui/material';
+
+const Dashboard = () => (
+    <Card>
+        <CardHeader title="VAIPE Drugs Administration" />
+        <CardContent>VAIPE Drugs Manage Website</CardContent>
+    </Card>
+);
+
+export default Dashboard;
